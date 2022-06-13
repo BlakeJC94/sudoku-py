9bad837778377b2ad6f7cf32c20771515fd11082
BlakeJC94 <blakejamescook@gmail.com>
2022-06-13 21:58:24 +1000

    feat: refactroed checkpointer

--- sudoku_py/checkpointer.py
+++ sudoku_py/checkpointer.py
@@ -1,6 +1,7 @@
+"""Classes and methods for saving checkpoints of sudoku puzzles."""
 from typing import List, Tuple, Dict
 
-from .sudoku import Sudoku
+from .sudoku import Puzzle
 
 
 class EmptyCheckpointer(Exception):
@@ -10,18 +11,24 @@ class EmptyCheckpointer(Exception):
 class Checkpointer:
     """Save states of a Sudoku puzzle."""
     def __init__(self):
-        """Constructor.
-
-        Args:
-        """ # TODO
+        """Constructor."""
         self.history = []
 
-    def stash(self, sudoku: Sudoku, guesses: Dict[int, List[int]]):
-        """"""  # TODO should make a guess in Solver before stashing
-        self.history.append((sudoku.copy(), guesses))
+    def stash(self, puzzle: Puzzle, guesses: Dict[int, List[int]]):
+        """Creates checkpoint for a (puzzle, guesses) pair.
 
-    def pop(self) -> Tuple[Sudoku, Dict[int, List[int]]]:
-        """"""  # TODO
+        Args:
+            puzzle: Sudoku puzzle to stash.
+            guesses: Dict of guesses to stash.
+        """
+        self.history.append((puzzle.copy(), guesses))
+
+    def pop(self) -> Tuple[Puzzle, Dict[int, List[int]]]:
+        """Remove and return last saved (puzzle, guesses) pair.
+
+        Returns:
+            Last saved (puzzle, guesses) pair.
+        """
         if len(self.history) == 0:
             raise EmptyCheckpointer("Attempted pop of empty Checkpointer")
 
