df23eee382ba99c5347d8c071871d952929f5779
BlakeJC94 <blakejamescook@gmail.com>
2022-06-13 21:58:44 +1000

    feat: refactored solver

--- sudoku_py/solver.py
+++ sudoku_py/solver.py
@@ -1,108 +1,122 @@
-from typing import List
+"""Classes and methods for solving sudoku puzzles."""
+from typing import List, Tuple, Dict
 
-from .sudoku import Sudoku
+from .sudoku import Puzzle
 from .checkpointer import Checkpointer
 
 
 class Solver:
     """Manages solving operations when given a Sudoku object."""
-    def __init__(self, total_sweeps: int = 10000):
-        """Constructor
+    def __init__(self, max_loops: int = 10000, patience: int = 2):
+        """Constructor.
 
         Args:
-        TODO
+            max_loops: number of loops to do over every cell before giving up.
+            patience: number of loops without changes to wait before guessing.
         """
-        self.total_sweeps = total_sweeps  # TODO rename to max_iterations
+        self.max_loops = max_loops
+        self.patience = patience
         self.checkpointer = Checkpointer()
 
-    # TODO combine get_row, get_col, get_block in Sudoku
-    # TODO move this method to Sudoku
-    def get_possibilities(self, sudoku: Sudoku, index: int) -> List[int]:
-        """""" # TODO
-        possibilities = set(range(1, sudoku.size + 1))
+    def solve(self, puzzle: Puzzle) -> Puzzle:
+        """Solves the sudoku puzzle.
 
-        row = set(sudoku.get_row(index))
-        col = set(sudoku.get_col(index))
-        block = set(sudoku.get_block(index))
+        Guesses are managed per loop in a dictionary that maps puzzle indices
+        to a list of possible values.
 
-        present = block.union(row).union(col)
-        return list(possibilities.difference(present))
+        Args:
+            puzzle: Unsolved sudoku puzzle
 
-    # TODO change `possibility_sweep` to a dict (or list of tuples?)
-    # TODO implement __getitem__ in Sudoku
-    # TODO implment `solved` attritute in Sudoku
-    # TODO refactor solve method
-    def solve(self, sudoku: Sudoku):
-        """""" # TODO
+        Returns:
+            Fully solved puzzle if successful, unsolved puzzle if unsuccessful.
+        """
         print("Solving puzzle")
-        print(sudoku)
-        sweep, sweep_num = True, 0
-        while sweep:  # TODO replace with for loop
-            # start sweep
+        print(puzzle)
+
+        loops_without_changes = 0
+        for loop in range(self.max_loops):
+
+            guesses = {}
             change_made = False
-            sweep_num += 1
-            if sweep_num > self.total_sweeps:
-                print("exceeded total_sweeps")
-                break
+            for index in puzzle.get_empty_indices():
+                options = puzzle.get_options(index)
 
-            possibility_sweep = [''] * len(sudoku.puzzle)
-            # TODO implement `get_empty_cells` in Sudoku and change this loop
-            for puzzle_index in range(len(possibility_sweep)):
-                if sudoku.puzzle[puzzle_index] == 0:
-
-                    possibilities = self.get_possibilities(
-                        sudoku, puzzle_index)
-
-                    if len(possibilities) > 1:
-                        possibility_sweep[puzzle_index] = possibilities
-
-                    elif len(possibilities) == 1:
-                        sudoku.puzzle[puzzle_index] = possibilities[0]
-                        change_made = True
-
-                    elif len(possibilities) == 0:
-                        print("INDETERMENENT, restore previous guess")
-                        sudoku, possibility_sweep = self.checkpointer.pop()
-                        # if one possibility in checkpoint, make change
-                        single_counts = {
-                            k: v
-                            for k, v in possibility_sweep.items()
-                            if len(v) == 1
-                        }
-                        if len(single_counts) > 0:
-                            # breakpoint()
-                            # TODO!! change poss checkpoint to tuiple
-                            selected_index = possibility_counts.index(1)
-                            possibilities = possibility_sweep[selected_index]
-                            sudoku.puzzle[selected_index] = possibilities[0]
-                            change_made = True
-
+                if len(options) == 0:
+                    print("INDETERMENENT, restore previous guess")
+                    puzzle, guesses, changed_when_restored = self.restore()
+                    if not changed_when_restored:
                         break
 
-            print(f"\nCompleted sweep {sweep_num}")
+                if len(options) == 1:
+                    puzzle[index] = options[0]
+                    change_made = True
+
+                if len(options) > 1:
+                    guesses[index] = options
+
+            print(f"\nCompleted loop {loop}")
+            print(puzzle)
+            if puzzle.is_solved():
+                break
 
             if not change_made:
-                if min(sudoku.puzzle) > 0:
-                    print("Puzzle solved!")
-                    print(sudoku)
-                    sweep = False
-                else:
-                    print("Multiple possibilities")
-                    print(sudoku)
-                    sudoku = self.guess(sudoku, possibility_sweep)
+                loops_without_changes += 1
 
-    # TODO refactor this guff
-    def guess(self, sudoku, possibility_sweep):
-        """""" # TODO
+            if loops_without_changes >= self.patience:
+                print("Multiple possibilities")
+                puzzle = self.guess(puzzle, guesses)
+
+        if not puzzle.is_solved():
+            print("Unsolved after reaching maximum number of loops.")
+
+        return puzzle
+
+    def guess(
+        self,
+        puzzle: Puzzle,
+        guesses: List[Tuple[int, List[int]]],
+    ) -> Puzzle:
+        """Select cell with minimal number of options and estimate value
+        before creating a checkpoint.
+
+        Possibility selected will be removed before creating a checkpoint.
+
+        Args:
+            puzzle: unsolved puzzle.
+            guesses: dictionary mapping puzzle indices to possibilities.
+
+        Returns:
+            Puzzle with one cell estimated.
+        """
         # find puzzle index with smallest number of possibilities
-        possibility_counts = list(map(len, possibility_sweep))
-        non_zero_counts = [i for i in possibility_counts if i != 0]
-        selected_puzzle_index = possibility_counts.index(min(non_zero_counts))
+        min_count = min(len(v) for v in guesses.values())
+        index = next(k for k, v in guesses.items() if len(v) == min_count)
+
         # get guess and remove from possibilities
-        guess = possibility_sweep[selected_puzzle_index][-1]
-        possibility_sweep[selected_puzzle_index].pop()
-        # save state
-        self.checkpointer.stash(sudoku, possibility_sweep)
+        opts = guesses[index]
+        guess = opts.pop()
+        guesses[index] = opts  # This might be a bit of paranoia
+
+        # save checkpoint
+        self.checkpointer.stash(puzzle, guesses)
+
         # insert guess
-        sudoku.puzzle[selected_puzzle_index] = guess
-        return sudoku
+        puzzle[index] = guess
+        return puzzle
+
+    def restore(self) -> Tuple[Puzzle, Dict[int, List[int]], bool]:
+        """Restore the last checkpoint and fill in cells that only have a single option.
+
+        Returns:
+            Restored puzzle, guesses, and flag to indicate if a change was made when restoring.
+        """
+        puzzle, guesses = self.checkpointer.pop()
+        changed_when_restored = False
+        try:
+            index = next(iter(k for k, v in guesses.items() if len(v) == 1))
+            puzzle[index] = guesses[index].pop()
+            changed_when_restored = True
+        except StopIteration:
+            pass
+
+        return puzzle, guesses, changed_when_restored
