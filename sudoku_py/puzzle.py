36737c6f557963aa45df684bc6bf218e380996ec
BlakeJC94 <blakejamescook@gmail.com>
2022-06-14 21:34:09 +1000

    fix: squashed bugs and added multiindex to puzzle

--- sudoku_py/puzzle.py
+++ sudoku_py/puzzle.py
@@ -2,8 +2,9 @@
 from __future__ import annotations
 
 import re
+from itertools import product
 from pathlib import Path
-from typing import List, Union
+from typing import List, Union, Tuple
 
 
 # TODO support loading puzzles as a list of rows
@@ -23,11 +24,13 @@ class Puzzle:
         self.data = self.load(input_data)
 
         assert len(self.data) in [16, 81], \
-            "`Sudoku` only supports puzzles of order 2 or 3."
+            "`Puzzle` only supports puzzles of order 2 or 3."
         self.order = 2 if len(self.data) == 16 else 3
 
         self.size = self.order ** 2  # row/col/block size
         self.total = self.size ** 2  # total number of elements in sudoku puzzle
+
+        assert self.is_valid(), "Expected a valid input."
 
     @staticmethod
     def load(input_data: Union[str, Path, List[int]]) -> List[int]:
@@ -52,10 +55,12 @@ class Puzzle:
 
         return puzzle
 
-    def __getitem__(self, index: int) -> int:
+    def __getitem__(self, index: Union[int, Tuple[int, int]]) -> int:
+        if isinstance(index, tuple) and len(index) == 2:
+            return self.data[index[0]*len(self) + index[1]]
         return self.data[index]
 
-    def __setitem__(self, index: int, element: int):
+    def __setitem__(self, index: Union[int, Tuple[int, int]], element: int):
         assert 0 <= element <= self.size, \
             f"Only allowed to set values between 0 and {self.size}."
         self.data[index] = element
@@ -122,8 +127,15 @@ class Puzzle:
         """Check if the puzzle has any empty cells."""
         return all(i != 0 for i in self.data)
 
-    def get_empty_indices(self) -> List[int]:
+    def get_empty_indices(
+        self,
+        ij_index: bool = False,
+    ) -> Union[List[int], List[Tuple[int,int]]]:
         """Get indices that correspond to empty cells."""
+        if ij_index:
+            ij_range = product(range(len(self)), range(len(self)))
+            return [(i, j) for i, j in ij_range if self[i, j] == 0]
+
         return [i for i in range(len(self) ** 2) if self[i] == 0]
 
     def _get_row_indices(self, index: int) -> List[int]:
@@ -132,7 +144,7 @@ class Puzzle:
 
     def _get_col_indices(self, index: int) -> List[int]:
         col_start = index % len(self)
-        return [col_start + len(self) * i for i in range(self)]
+        return [col_start + len(self) * i for i in range(len(self))]
 
     def _get_block_indices(self, index: int) -> List[int]:
         block_index = (self.order ** 3) * (index // self.order ** 3) \
@@ -142,30 +154,39 @@ class Puzzle:
             for i in range(len(self))
         ]
 
-    def get_row_col_block_indices(self, index: int) -> List[int]:
+    def _get_row_col_block_indices(self, index: int) -> List[int]:
         """Get indices of the elements in the same row, column, and block as
         the input index."""
         row_indices = self._get_row_indices(index)
         col_indices = self._get_col_indices(index)
         block_indices = self._get_block_indices(index)
-        return list(set(sum([row_indices, col_indices, block_indices])))
+        return list(set(sum([row_indices, col_indices, block_indices], [])))
 
-    def get_options(self, index: int) -> List[int]:
+    def get_options(self, index_i: int, index_j: Optional[int] = None) -> List[int]:
         """Get options for puzzle cell index."""
+        if index_j is not None:
+            index = index_i * len(self) + index_j
+        else:
+            index = index_i
+
         if self[index] != 0:
             return []
 
-        indices = self.get_row_col_block_indices(index)
+        indices = self._get_row_col_block_indices(index)
         elements = set(self[i] for i in indices if self[i] != 0)
         return [i + 1 for i in range(len(self)) if i + 1 not in elements]
 
     def is_valid(self) -> bool:
         """Check if the puzzle is valid."""
         valid = True
-        for index in self:
-            indices = self.get_row_col_block_indices(index)
-            values = [self[i] for i in indices if self[i] != 0]
-            if len(values) != len(set(values)):
+        empty_indices = self.get_empty_indices()
+        non_empty_indices = [
+            i for i in range(self.total) if i not in empty_indices
+        ]
+        for index in non_empty_indices:
+            indices = self._get_row_col_block_indices(index)
+            values = [self[i] for i in indices if self[i] != 0 and i != index]
+            if self[index] in values:
                 valid = False
                 break
 
